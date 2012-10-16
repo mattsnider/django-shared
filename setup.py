4e2a6306f0bced8e79e1fb5b160776116f3fb9bb
matt snider <msnider@votizen.com>
2012-10-16 14:55:33 -0700

    looks like OSX doesn't package correctly. using a hack from django-extensions setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import os, sys
+from distutils.command.install_data import install_data
+from distutils.command.install import INSTALL_SCHEMES
 from setuptools import setup, Command, find_packages
 
 
@@ -39,6 +41,47 @@ class QuickRunTests(RunTests):
     extra_env = dict(SKIP_RLIMITS=1, QUICKTEST=1)
 
 
+class osx_install_data(install_data):
+    # On MacOS, the platform-specific lib dir is at:
+    #   /System/Library/Framework/Python/.../
+    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific
+    # fix for this in distutils.command.install_data#306. It fixes install_lib
+    # but not install_data, which is why we roll our own install_data class.
+
+    def finalize_options(self):
+        # By the time finalize_options is called, install.install_lib is set to
+        # the fixed directory, so we set the installdir to install_lib. The
+        # install_data class uses ('install_data', 'install_dir') instead.
+        self.set_undefined_options('install', ('install_lib', 'install_dir'))
+        install_data.finalize_options(self)
+
+if sys.platform == "darwin":
+    cmdclasses = {'install_data': osx_install_data}
+else:
+    cmdclasses = {'install_data': install_data}
+
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+# Tell distutils to put the data_files in platform-specific installation
+# locations. See here for an explanation:
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+
 def fullsplit(path, result=None):
     """
     Split a pathname into components (the opposite of os.path.join) in a
@@ -72,6 +115,9 @@ for dirpath, dirnames, filenames in os.walk(package_dir):
         data_files.append(
             [dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
+cmdclasses['test'] = RunTests
+cmdclasses['quicktest'] = QuickRunTests
+
 setup(
     name = 'django-shared',
     packages=find_packages(),
@@ -86,10 +132,11 @@ setup(
     maintainer_email = 'admin@mattsnider.com',
     keywords = ['python', 'django'],
     license = 'MIT',
-    cmdclass={
-        "test": RunTests,
-        "quicktest": QuickRunTests
-    },
+    cmdclass=cmdclasses,
+#    cmdclass={
+#        "test": RunTests,
+#        "quicktest": QuickRunTests
+#    },
     include_package_data=True,
     install_requires=[
         'django>=1.3',
