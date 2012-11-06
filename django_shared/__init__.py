9ef30ec214d983e17ef941da9ab0de11d69b178b
matt snider <msnider@votizen.com>
2012-11-06 15:53:52 -0800

    improving packaging, upgrading to html5boilerplate 4.0.1, cleaning up HTML scaffolding

--- django_shared/__init__.py
+++ django_shared/__init__.py
@@ -1,2 +1,2 @@
 __author__ = 'mattesnider'
-__version__ = (1, 2, 1)
+__version__ = (1, 2, 2)
--- setup.py
+++ setup.py
@@ -1,142 +1,26 @@
 #!/usr/bin/env python
-
 import os, sys
-from distutils.command.install_data import install_data
-from distutils.command.install import INSTALL_SCHEMES
-from setuptools import setup, Command, find_packages
+from setuptools import setup, find_packages
 
+ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
 
-class RunTests(Command):
-    description = "Run the django test suite from the tests dir."
-    user_options = []
-    extra_env = {}
-    extra_args = []
+# README is required for distribution, but README.md is required for github,
+#   so create README temporarily
+os.system('cp %s/README.md %s/README.txt' % (ROOT_DIR, ROOT_DIR))
 
-    def run(self):
-        for env_name, env_value in self.extra_env.items():
-            os.environ[env_name] = str(env_value)
-
-        this_dir = os.getcwd()
-        testproj_dir = os.path.join(this_dir, "tests")
-        os.chdir(testproj_dir)
-        sys.path.append(testproj_dir)
-        from django.core.management import execute_from_command_line
-        os.environ["DJANGO_SETTINGS_MODULE"] = os.environ.get(
-            "DJANGO_SETTINGS_MODULE", "settings")
-        prev_argv = list(sys.argv)
-        try:
-            sys.argv = [__file__, "test"] + self.extra_args
-            execute_from_command_line(argv=sys.argv)
-        finally:
-            sys.argv = prev_argv
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-
-class QuickRunTests(RunTests):
-    extra_env = dict(SKIP_RLIMITS=1, QUICKTEST=1)
-
-
-class osx_install_data(install_data):
-    # On MacOS, the platform-specific lib dir is at:
-    #   /System/Library/Framework/Python/.../
-    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific
-    # fix for this in distutils.command.install_data#306. It fixes install_lib
-    # but not install_data, which is why we roll our own install_data class.
-
-    def finalize_options(self):
-        # By the time finalize_options is called, install.install_lib is set to
-        # the fixed directory, so we set the installdir to install_lib. The
-        # install_data class uses ('install_data', 'install_dir') instead.
-        self.set_undefined_options('install', ('install_lib', 'install_dir'))
-        install_data.finalize_options(self)
-
-if sys.platform == "darwin":
-    cmdclasses = {'install_data': osx_install_data}
-else:
-    cmdclasses = {'install_data': install_data}
-
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-package_dir = 'django_shared'
-
-for dirpath, dirnames, filenames in os.walk(package_dir):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'):
-            del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append(
-            [dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
-cmdclasses['test'] = RunTests
-cmdclasses['quicktest'] = QuickRunTests
-
-setup(
+sdict = dict(
     name = 'django-shared',
-    packages=find_packages(),
-    data_files=data_files,
-    version='.'.join(map(str, __import__(package_dir).__version__)),
+    packages = find_packages(),
+    version='.'.join(map(str, __import__('django_shared').__version__)),
     description = 'Common tools for working with Django and Python.',
-    long_description = open("README.md", "r").read(),
-    url = 'http://github.com/votizen/django-shared',
+    long_description=open('README.md').read(),
+    url = 'https://github.com/mattsnider/django-shared',
     author = 'Matt Snider',
     author_email = 'admin@mattsnider.com',
     maintainer = 'Matt Snider',
     maintainer_email = 'admin@mattsnider.com',
     keywords = ['python', 'django'],
     license = 'MIT',
-    cmdclass=cmdclasses,
-#    cmdclass={
-#        "test": RunTests,
-#        "quicktest": QuickRunTests
-#    },
     include_package_data=True,
     install_requires=[
         'django>=1.3',
@@ -149,8 +33,13 @@ setup(
         'Operating System :: OS Independent',
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
-        'Framework :: Django',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
-      ]
+    ]
 )
+
+from distutils.core import setup
+setup(**sdict)
+
+# cleanup README
+os.remove('%s/README.txt' % ROOT_DIR)
