1055b71e91328e4b868f412a46c642e8f7223909
matt snider <msnider@votizen.com>
2012-11-13 12:25:57 -0800

    adding gzip url handler and an ajax_only decorator

--- django_shared/http/__init__.py
+++ django_shared/http/__init__.py
@@ -1,11 +1,23 @@
-from django.http import HttpResponse
+from StringIO import StringIO
+import gzip
+import httplib
+import string
+import urllib
+import urllib2
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.utils import simplejson
+
+
+def dict_lower(d):
+    return dict(zip(map(string.lower, d.keys()), map(
+        string.lower, d.values())))
+
 
 class JsonResponse(HttpResponse):
     """
     Encapsulates a JSON response, to keep code DRY.
     """
-    def __init__(self, data=None, errors=[ ], success=True):
+    def __init__(self, data=None, errors=None, success=True):
         """
         data is a map, errors a list
         """
@@ -14,9 +26,56 @@ class JsonResponse(HttpResponse):
         json = json_response(data=data, errors=errors, success=success)
         super(JsonResponse, self).__init__(json, mimetype='application/json')
 
-def json_response(data={ }, errors=[ ], success=True):
+
+def json_response(data=None, errors=None, success=True):
     data.update({
         'errors': errors,
-        'success': bool(len(errors) == 0 and success),
-        })
+        'success': not errors and success,
+    })
     return simplejson.dumps(data)
+
+
+def _gzip_open(handler, req, cls):
+    """
+    Adapted from: https://derrickpetzold.com/index.php/python-gzip-http/
+    """
+    req.add_header('Accept-Encoding', 'gzip')
+    r = handler.do_open(cls, req)
+    headers = dict_lower(r.headers)
+    if 'content-encoding' in headers and headers['content-encoding'] == 'gzip':
+        fp = gzip.GzipFile(fileobj=StringIO(r.read()))
+    else:
+        fp = r
+    resp = urllib.addinfourl(fp, r.headers, r.url, r.code)
+    resp.msg = r.msg
+    return resp
+
+
+class GzipHttpHandler(urllib2.HTTPHandler):
+    def http_open(self, req):
+        return _gzip_open(self, req, httplib.HTTPConnection)
+
+
+class GzipHttpsHandler(urllib2.HTTPSHandler):
+    def https_open(self, req):
+        return _gzip_open(self, req, httplib.HTTPSConnection)
+
+
+def gzip_urlopen(url, data=None, timeout=30):
+    """
+    Extend urllib2.urlopen() to support gzip encoding.
+    """
+    request = urllib2.Request(url)
+    opener = urllib2.build_opener(GzipHttpHandler, GzipHttpsHandler)
+    return opener.open(request, data, timeout)
+
+
+def ajax_only(view_func):
+    """
+    Decorator for requests that are only supposed to handle AJAX requests.
+    """
+    def inner(request, *args, **kwargs):
+        if not request.is_ajax():
+            return HttpResponseBadRequest()
+        return view_func(request, *args, **kwargs)
+    return inner
