6604fa6bb7e3dde74bd08e4b905bff8ba589d3c6
mattsnider <mattesnider@gmail.com>
2013-01-16 11:51:45 -0800

    adding memoized_property

--- django_shared/models.py
+++ django_shared/models.py
@@ -20,6 +20,24 @@ def keyify_obj(o):
     return keyify(o.content_type.pk, o.pk)
 
 
+class memoized_property(object):
+    """
+    A read-only @property that is only evaluated once.
+
+    From: http://www.reddit.com/r/Python/comments/ejp25/cached_property_decorator_that_is_memory_friendly/
+    """
+    def __init__(self, fget, doc=None):
+        self.fget = fget
+        self.__doc__ = doc or fget.__doc__
+        self.__name__ = fget.__name__
+
+    def __get__(self, obj, cls):
+        if obj is None:
+            return self
+        obj.__dict__[self.__name__] = result = self.fget(obj)
+        return result
+
+
 class ModelBase(models.Model):
     """
     An abstract model to be the foundation for all other models. Adds
